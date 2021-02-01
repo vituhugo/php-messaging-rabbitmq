aabadebcfcf6232b4aad80135439b84e1c9125aa
Victor Rodrigues <victor.rodrigues@nexxera.com>
2021-02-01 05:11:52 -0300

    corigido delivery info

--- src/Core/Fabricas/FabricaConsumivel.php
+++ src/Core/Fabricas/FabricaConsumivel.php
@@ -10,7 +10,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 class FabricaConsumivel
 {
     public static function fabricarComAMQPMessage(AMQPMessage $mensagem) {
-        $req = new Consumivel($mensagem->body, $mensagem->getRoutingKey());
+        $req = new Consumivel($mensagem->body, $mensagem->delivery_info['routing_key']);
         $req->setProperties($mensagem->get_properties());
         $req->setHeaders($mensagem->has('application_headers') ? $mensagem->get('application_headers') : array());
 
